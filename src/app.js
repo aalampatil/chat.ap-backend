d8af1ed3087ce486ed19674ee4362fd5eeac76a8
Aalam <aalam.workacc@email.com>
2025-10-28 11:58:40 +0530

    refactor: enhance CORS middleware to allow specific origins

--- src/app.js
+++ src/app.js
@@ -4,13 +4,24 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-//middlewares
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true, //required for sending header in request
-  })
-);
+const allowedOrigins = [
+  process.env.CORS_ORIGIN,
+  `http://localhost:${process.env.PORT}`
+];
+
+//
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
+
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true, limit: "4mb" }));
 app.use(express.static("public"));
