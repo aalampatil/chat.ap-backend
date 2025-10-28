fcd27d53e3e3797e9dfe6b3c0412f2a531e64402
Aalam <aalam.workacc@email.com>
2025-10-28 12:01:24 +0530

    fix: update MongoDB connection URI to remove unnecessary path

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
    try {
-      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/chatAp`)
+      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
       console.log(`\n mongoDB connected !! dbHost: ${connectionInstance.connection.host}`);
       
    } catch (error) {
